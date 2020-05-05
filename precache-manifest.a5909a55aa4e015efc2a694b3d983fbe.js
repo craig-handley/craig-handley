9a0ad53b7801809f987abc78843b1fc1af600bb9
clicktravel-craig <craig.handley@clicktravel.com>
2020-05-05 13:17:32 +0100

    Updates

---
+++ precache-manifest.a5909a55aa4e015efc2a694b3d983fbe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/craig-handley/favicon.ico"
+  },
+  {
+    "revision": "36645583ef92962fd512f2596d995078",
+    "url": "/craig-handley/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/craig-handley/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/craig-handley/manifest.json"
+  },
+  {
+    "revision": "75ef6fc54c0cc6f02714",
+    "url": "/craig-handley/static/css/main.f7125c40.chunk.css"
+  },
+  {
+    "revision": "75ef6fc54c0cc6f02714",
+    "url": "/craig-handley/static/js/main.75ef6fc5.chunk.js"
+  },
+  {
+    "revision": "aeb6eef3e495f6cfe046",
+    "url": "/craig-handley/static/js/runtime~main.aeb6eef3.js"
+  },
+  {
+    "revision": "cf654020784bccbfc83d",
+    "url": "/craig-handley/static/js/vendors~main.cf654020.chunk.js"
+  },
+  {
+    "revision": "d4b6a229d7ae188fd8510dfc7a797da2",
+    "url": "/craig-handley/static/media/home.d4b6a229.jpg"
+  },
+  {
+    "revision": "35824288332cedcdf6ddb0c12aaccf86",
+    "url": "/craig-handley/static/media/resume.35824288.jpg"
+  }
+]);
